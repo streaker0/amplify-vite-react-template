6b07ec41755492ff530b0b197aa6289fad294364
streaker0 <mujeebakinbile@hotmail.com>
2024-12-26 15:14:24 -0500

    created ui interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,43 @@
 import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+	const { signOut } = useAuthenticator();
+	const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+	useEffect(() => {
+		client.models.Todo.observeQuery().subscribe({
+			next: (data) => setTodos([...data.items]),
+		});
+		<button onClick={signOut}>Sign out</button>
+	}, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+	function createTodo() {
+		client.models.Todo.create({ content: window.prompt("Todo content") });
+	}
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<h1>My todos</h1>
+			<button onClick={createTodo}>+ new</button>
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>{todo.content}</li>
+				))}
+			</ul>
+			<div>
+				🥳 App successfully hosted. Try creating a new todo.
+				<br />
+				<a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+					Review next step of this tutorial.
+				</a>
+			</div>
+		</main>
+	);
 }
 
 export default App;
