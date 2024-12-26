6aaa4a7e2c81db8c6a134679c03fb8bb8cbe3e9c
streaker0 <mujeebakinbile@hotmail.com>
2024-12-26 15:10:40 -0500

    made to username only auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,11 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const { cfnUserPool } = backend.auth.resources.cfnResources
+// an empty array denotes "email" and "phone_number" cannot be used as a username
+cfnUserPool.usernameAttributes = []
